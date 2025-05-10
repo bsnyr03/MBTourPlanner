65c0e985701f1ba4f759070d44f54275ed934af8
Baris Senyer <barisenyer@icloud.com>
2025-05-10 16:39:33 +0200

    initializing commit

---
+++ src/main/java/at/fhtw/mbtourplanner/MbTourPlannerApplication.java
@@ -0,0 +1,13 @@
+package at.fhtw.mbtourplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbTourPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MbTourPlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/fhtw/mbtourplanner/MbTourPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhtw.mbtourplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbTourPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
