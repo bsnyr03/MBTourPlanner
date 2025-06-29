49f74a4b7af0686d20fea873638b276e12303c81
Baris Senyer <barisenyer@icloud.com>
2025-06-29 18:45:14 +0200

    first TourService Unit Test works

--- src/test/java/at/fhtw/mbtourplanner/TourControllerTest.java
+++ src/test/java/at/fhtw/mbtourplanner/TourControllerTest.java
@@ -4,9 +4,9 @@ import at.fhtw.mbtourplanner.controller.TourController;
 import at.fhtw.mbtourplanner.model.Tour;
 import at.fhtw.mbtourplanner.service.TourService;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -23,7 +23,7 @@ class TourControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @Mock
     private TourService tourService;
 
     @Test
---
+++ src/test/java/at/fhtw/mbtourplanner/TourServiceTest.java
@@ -0,0 +1,82 @@
+package at.fhtw.mbtourplanner;
+
+
+import at.fhtw.mbtourplanner.model.Tour;
+import at.fhtw.mbtourplanner.repository.TourEntity;
+import at.fhtw.mbtourplanner.repository.TourLogRepository;
+import at.fhtw.mbtourplanner.repository.TourRepository;
+import at.fhtw.mbtourplanner.service.TourMapper;
+import at.fhtw.mbtourplanner.service.TourService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+
+import java.sql.SQLException;
+import java.time.Duration;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
+public class TourServiceTest {
+
+    @Mock
+    private TourRepository tourRepository;
+
+    @Mock private TourLogRepository tourLogRepository;
+
+    @Mock private TourMapper tourMapper;
+
+    @InjectMocks
+    private TourService tourService;
+
+    private TourEntity sampleEntity;
+    private Tour sampleDto;
+
+    @BeforeEach
+    void setUp() throws SQLException {
+        sampleEntity = new TourEntity();
+        sampleEntity.setId(1L);
+        sampleEntity.setName("Sample Name");
+        sampleEntity.setDescription("Sample Description");
+        sampleEntity.setFromLocation("Sample From");
+        sampleEntity.setToLocation("Sample To");
+        sampleEntity.setTransportType("Car");
+        sampleEntity.setDistance(1000);
+        sampleEntity.setEstimatedTime(Duration.ofHours(1));
+        sampleEntity.setRouteImageUrl("Sample Route Image");
+
+
+        sampleDto = Tour.builder().name("Sample").build();
+
+        when(tourMapper.toDto(sampleEntity)).thenReturn(sampleDto);
+        when(tourMapper.toDto(anyList())).thenReturn(List.of(sampleDto));
+        when(tourMapper.toEntity(sampleDto)).thenReturn(sampleEntity);
+    }
+
+    @Test
+    void getAllTours_shouldMapAllEntities() throws SQLException {
+        when(tourRepository.findAll()).thenReturn(List.of(sampleEntity));
+
+        var result = tourService.getAllTours();
+
+        assertThat(result)
+                .hasSize(1)
+                .first().isSameAs(sampleDto);
+
+        verify(tourRepository).findAll();
+    }
+
+
+
+
+}
